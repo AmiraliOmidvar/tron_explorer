e87011a3d07c6194c80108e90b3e8ed908c71fa2
Amirali <amir.om35@gmail.com>
2022-11-17 02:44:55 +0430

    added python version requirement

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='1.0.0',
     packages=['tron_explorer', 'tron_explorer.test'],
     url='',
+    python_requires='>3.8',
     license='MIT',
     author='Amirali Omidvar',
     author_email='amirali.omidvar80@gmail.com',
